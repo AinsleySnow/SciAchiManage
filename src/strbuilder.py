002a752640aa75a08e1e5740dd5e8aae50ca5beb
AinsleySnow <AinsleySnow@outlook.com>
2023-02-09 16:54:28 +0800

    add new method 'extend'

--- src/strbuilder.py
+++ src/strbuilder.py
@@ -1,14 +1,17 @@
 class StringBuilder:
     def __init__(self, args: list = None) -> None:
-        self.arglist = []
+        self.__arglist_ = []
         if args:
-            self.arglist.extend(args)
+            self.__arglist_.extend(args)
 
     def __str__(self) -> str:
         return self.join()
 
     def append(self, *ele) -> None:
-        self.arglist.extend(ele)
+        self.__arglist_.extend(*ele)
+
+    def extend(self, eles: list) -> None:
+        self.__arglist_.extend(eles)
 
     def build(self) -> str:
-        return ''.join(*self.arglist)
+        return ''.join(*self.__arglist_)
