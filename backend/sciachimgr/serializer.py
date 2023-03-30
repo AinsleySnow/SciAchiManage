0e66f3231b91d7990fc6d5c4e8851f5b9aba8620
AinsleySnow <AinsleySnow@outlook.com>
2023-03-30 09:02:46 +0800

    enable crud for np, conf, jour in backend

--- backend/sciachimgr/serializer.py
+++ backend/sciachimgr/serializer.py
@@ -10,3 +10,24 @@ class ResearcherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Researcher
         fields = ['id', 'dept', 'position', 'profile', 'work', 'photo']
+
+class CollegeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = College
+        fields = ['id', 'name']
+
+
+class JournalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Journal
+        fields = ['issn', 'title', 'host', 'period', 'inf_factor', 'zone', 'picture', 'link']
+
+class NewspaperSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Newspaper
+        fields = ['issn', 'title', 'authority', 'host', 'city', 'address', 'postcode', 'phone_num', 'picture', 'link']
+
+class ConferenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Conference
+        fields = ['id', 'name', 'time', 'place', 'association', 'publisher', 'publish_date', 'chief_editor', 'link']
--- backend/sciachimgr/views.py
+++ backend/sciachimgr/views.py
@@ -200,3 +200,391 @@ def DeleteUser(request):
         return HttpResponse('success')
     except:
         return Response(status=403)
+
+
+class CollegeList(APIView):
+    serializer_class = CollegeSerializer
+
+    def get(self, request):
+        college = [
+            {
+                'id': c.id,
+                'name': c.name
+            }
+            for c in College.objects.all()
+        ]
+        return Response(college)
+
+    def post(self, request):
+        serializer = CollegeSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+
+class CollegeMembers(APIView):
+    serializer_class = UserSerializer
+
+    def get(self, request):
+        name = College.objects.get(id=request.GET['id']).name
+        user = [
+            {
+                'id': user.id,
+                'type': user.type,
+                'name': user.name,
+                'passwd': user.passwd,
+                'sex': user.sex,
+                'dept': user.dept
+            }
+            for user in User.objects.filter(dept=name)
+        ]
+        return Response(user)
+   
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+
+def AddCollege(request):
+    received = json.loads(request.body)
+    curusr = received.get('curusr')
+    toadd = received.get('toadd')
+
+    if curusr[2:4] != '03':
+        return Response(status=403)
+
+    College.objects.create(name=toadd)
+    return HttpResponse('success')
+
+
+def DeleteCollege(request):
+    received = json.loads(request.body)
+    curusr = received.get('curusr')
+    todelete = received.get('todelete')
+
+    if curusr[2:4] != '03':
+        return Response(status=403)
+
+    College.objects.filter(id=todelete).delete()
+    id = '%02d' % int(todelete)
+    Researcher.objects.filter(rid__id__startswith=id).delete()
+    User.objects.filter(id__startswith=id).delete()
+
+    return HttpResponse('success')
+
+
+class NewspaperInfo(APIView):
+    serializer_class = UserSerializer
+
+    def get(self, request):
+        issn = request.GET['id']
+        if issn != 'all':
+            np = Newspaper.objects.get(issn=issn)
+            return Response({
+                'issn': np.issn,
+                'title': np.title,
+                'authority': np.authority,
+                'host': np.host,
+                'city': np.city,
+                'address': np.address,
+                'postcode': np.postcode,
+                'phone_num': np.phone_num,
+                'picture': np.picture.name,
+                'link': np.link
+            })
+        else:
+            newspaper = [
+                {
+                    'issn': np.issn,
+                    'title': np.title,
+                    'authority': np.authority,
+                    'host': np.host,
+                    'city': np.city,
+                    'address': np.address,
+                    'postcode': np.postcode,
+                    'phone_num': np.phone_num,
+                    'picture': np.picture.name,
+                    'link': np.link
+                }
+                for np in Newspaper.objects
+            ]
+            return Response(newspaper)
+
+    def post(self, request):
+        serializer = NewspaperSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+
+def SetNewspaperInfo(request):
+    received = json.loads(request.body)
+    issn = received.get('issn')
+
+    np = Newspaper.objects.get(issn=issn)
+    if np.issn != received.get('issn'):
+        np.issn = received.get('issn')
+    if np.title != received.get('title'):
+        np.title = received.get('title')
+    if np.authority != received.get('authority'):
+        np.authority = received.get('authority')
+    if np.host != received.get('host'):
+        np.host = received.get('host')
+    if np.city != received.get('city'):
+        np.city = received.get('city')
+    if np.address != received.get('address'):
+        np.address = received.get('address')
+    if np.postcode != received.get('postcode'):
+        np.postcode = received.get('postcode')
+    if np.phone_num != received.get('phone_num'):
+        np.phone_num = received.get('phone_num')
+    if np.picture.name != received.get('picture'):
+        np.picture = received.get('picture')
+
+    np.save()
+    return HttpResponse('success')
+
+
+def AddNewspaper(request):
+    received = json.loads(request.body)
+    curusr = received.get('uid')
+    if curusr[2:4] != '03':
+        return Response(status=403)
+
+    Newspaper.objects.create(
+        issn = received.get('issn'),
+        title = received.get('title'),
+        authority = received.get('authority'),
+        host = received.get('host'),
+        city = received.get('city'),
+        address = received.get('address'),
+        postcode = received.get('postcode'),
+        phone_num = received.get('phone_num'),
+        picture = received.get('picture'),
+        link = received.get('link')
+    )
+
+    return HttpResponse('success')
+
+
+def DeleteNewspaper(request):
+    received = json.loads(request.body)
+    try:
+        curusr = received.get('curusr')
+        if curusr[2:4] != '03':
+            return Response(status=403)
+
+        todelete = received.get('todelete')
+        Newspaper.objects.filter(issn=todelete).delete()
+        return HttpResponse('success')
+    except:
+        return Response(status=403)
+
+
+class JournalInfo(APIView):
+    serializer_class = JournalSerializer
+
+    def get(self, request):
+        issn = request.GET['id']
+        if issn != 'all':
+            journal = Journal.objects.get(issn=issn)
+            return Response({
+                'issn': journal.issn,
+                'title': journal.title,
+                'host': journal.host,
+                'period': journal.period,
+                'inf_factor': journal.inf_factor,
+                'zone': journal.zone,
+                'picture': journal.picture.name,
+                'link': journal.link
+            })
+        else:
+            journal = [
+                {
+                    'issn': j.issn,
+                    'title': j.title,
+                    'host': j.host,
+                    'period': j.period,
+                    'inf_factor': j.inf_factor,
+                    'zone': j.zone,
+                    'picture': j.picture.name,
+                    'link': j.link
+                }
+                for j in Journal.objects
+            ]
+            return Response(journal)
+
+    def post(self, request):
+        serializer = JournalSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+
+def SetJournalInfo(request):
+    received = json.loads(request.body)
+    issn = received.get('issn')
+
+    j = Newspaper.objects.get(issn=issn)
+    if j.issn != received.get('issn'):
+        j.issn = received.get('issn')
+    if j.title != received.get('title'):
+        j.title = received.get('title')
+    if j.host != received.get('host'):
+        j.host = received.get('host')
+    if j.period != received.get('period'):
+        j.period = received.get('period')
+    if j.inf_factor != received.get('inf_factor'):
+        j.inf_factor = received.get('inf_factor')
+    if j.zone != received.get('zone'):
+        j.zone = received.get('zone')
+    if j.picture.name != received.get('picture'):
+        j.picture = received.get('picture')
+    if j.link != received.get('link'):
+        j.link = received.get('link')
+
+    j.save()
+    return HttpResponse('success')
+
+
+def AddJournal(request):
+    received = json.loads(request.body)
+    curusr = received.get('uid')
+    if curusr[2:4] != '03':
+        return Response(status=403)
+
+    Journal.objects.create(
+        issn = received.get('issn'),
+        title = received.get('title'),
+        host = received.get('host'),
+        period = received.get('period'),
+        inf_factor = received.get('inf_factor'),
+        zone = received.get('zone'),
+        picture = received.get('picture'),
+        link = received.get('link')
+    )
+
+    return HttpResponse('success')
+
+
+def DeleteJournal(request):
+    received = json.loads(request.body)
+    try:
+        curusr = received.get('curusr')
+        if curusr[2:4] != '03':
+            return Response(status=403)
+
+        todelete = received.get('todelete')
+        Journal.objects.filter(issn=todelete).delete()
+        return HttpResponse('success')
+    except:
+        return Response(status=403)
+
+
+class ConferenceInfo(APIView):
+    serializer_class = UserSerializer
+
+    def get(self, request):
+        id = request.GET['id']
+        if id != 'all':
+            conf = Conference.objects.get(id=id)
+            return Response({
+                'id': conf.id,
+                'name': conf.name,
+                'time': conf.time,
+                'place': conf.place,
+                'association': conf.association,
+                'publisher': conf.publisher,
+                'publish_date': conf.publish_date,
+                'chief_editor': conf.chief_editor,
+                'editors': conf.editors,
+                'link': conf.link
+            })
+        else:
+            conference = [
+                {
+                    'id': conf.id,
+                    'name': conf.name,
+                    'time': conf.time,
+                    'place': conf.place,
+                    'association': conf.association,
+                    'publisher': conf.publisher,
+                    'publish_date': conf.publish_date,
+                    'chief_editor': conf.chief_editor,
+                    'editors': conf.editors,
+                    'link': conf.link
+                }
+                for conf in Conference.objects
+            ]
+            return Response(conference)
+
+    def post(self, request):
+        serializer = NewspaperSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+
+def SetConferenceInfo(request):
+    received = json.loads(request.body)
+    id = received.get('id')
+
+    conf = Conference.objects.get(id=id)
+    if conf.id != received.get('id'):
+        conf.id = received.get('id')
+    if conf.title != received.get('title'):
+        conf.title = received.get('title')
+    if conf.authority != received.get('authority'):
+        conf.authority = received.get('authority')
+    if conf.host != received.get('host'):
+        conf.host = received.get('host')
+    if conf.city != received.get('city'):
+        conf.city = received.get('city')
+    if conf.address != received.get('address'):
+        conf.address = received.get('address')
+    if conf.postcode != received.get('postcode'):
+        conf.postcode = received.get('postcode')
+    if conf.phone_num != received.get('phone_num'):
+        conf.phone_num = received.get('phone_num')
+    if conf.picture.name != received.get('picture'):
+        conf.picture = received.get('picture')
+
+    conf.save()
+    return HttpResponse('success')
+
+
+def AddConference(request):
+    received = json.loads(request.body)
+    curusr = received.get('uid')
+    if curusr[2:4] != '03':
+        return Response(status=403)
+
+    Conference.objects.create(
+        name = received.get('name'),
+        time = received.get('time'),
+        place = received.get('place'),
+        association = received.get('association'),
+        publisher = received.get('publisher'),
+        publish_date = received.get('publish_date'),
+        chief_editor = received.get('chief_editor'),
+        editors = received.get('editors'),
+        link = received.get('link')
+    )
+
+    return HttpResponse('success')
+
+
+def DeleteConference(request):
+    received = json.loads(request.body)
+    try:
+        curusr = received.get('curusr')
+        if curusr[2:4] != '03':
+            return Response(status=403)
+
+        todelete = received.get('todelete')
+        Conference.objects.filter(issn=todelete).delete()
+        return HttpResponse('success')
+    except:
+        return Response(status=403)
